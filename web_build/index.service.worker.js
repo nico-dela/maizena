21f0fa971c0ad3e86f68f4abc8f135acdff0e00e
delanou <delacruzbordajn@gmail.com>
2026-02-19 18:31:18 -0300

    t
    nuevo build 19022026183125

--- web_build/index.service.worker.js
+++ web_build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771535789|1221201171';
+const CACHE_VERSION = '1771536529|1961597768';
 /** @type {string} */
 const CACHE_PREFIX = 'Maizena Web Game-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
