d0b325e78b8498aa09e883b3a74e628068437610
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2026-06-17 21:43:31 -0300

    Remove unused texture import files for various image sizes and update service worker cache version for improved resource management.

--- web_build/index.service.worker.js
+++ web_build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781742308|11351880925';
+const CACHE_VERSION = '1781743392|12435986574';
 /** @type {string} */
 const CACHE_PREFIX = 'Archipiélago Mai-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
