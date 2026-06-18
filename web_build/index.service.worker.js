88a62ae24df930850cf19bfc013ca14aaf2cf9ac
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2026-06-17 21:25:18 -0300

    Update service worker cache version to improve resource management.

--- web_build/index.service.worker.js
+++ web_build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781742108|11152175654';
+const CACHE_VERSION = '1781742308|11351880925';
 /** @type {string} */
 const CACHE_PREFIX = 'Archipiélago Mai-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
