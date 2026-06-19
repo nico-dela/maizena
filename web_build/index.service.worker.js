895978add83434237ff1c870110f885defed855a
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2026-06-18 21:25:24 -0300

    Update GODOT configuration with new file sizes and increment service worker cache version for improved resource management.

--- web_build/index.service.worker.js
+++ web_build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781755954|3492437006';
+const CACHE_VERSION = '1781828712|6774421613';
 /** @type {string} */
 const CACHE_PREFIX = 'Archipiélago Mai-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
