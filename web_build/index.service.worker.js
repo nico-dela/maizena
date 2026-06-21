c1552f9927794e4ad048f62b0e4676d7d7440709
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2026-06-21 01:05:55 -0300

    Update web build configuration: adjust GODOT_CONFIG file sizes for consistency and increment CACHE_VERSION in service worker for updated resource caching.

--- web_build/index.service.worker.js
+++ web_build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782014228|2204637033';
+const CACHE_VERSION = '1782014738|5194014';
 /** @type {string} */
 const CACHE_PREFIX = 'Archipiélago Mai-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
