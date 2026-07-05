bae7ff09517a37b316ebc6c4e6141b5c457f0cd7
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2026-07-04 22:05:06 -0300

    Remove unused texture import files and update index.html to reflect the correct file size for index.pck. Increment CACHE_VERSION in the service worker for improved caching.

--- web_build/index.service.worker.js
+++ web_build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783212886|11068111253';
+const CACHE_VERSION = '1783213486|11668085314';
 /** @type {string} */
 const CACHE_PREFIX = 'Archipiélago Mai-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
