78b22f534664d905b44e310b7567da0662d3ca15
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2026-07-04 21:55:37 -0300

    Update web build files: Adjust GODOT_CONFIG file size for index.pck, increment CACHE_VERSION in service worker for improved caching, and ensure consistency in resource management.

--- web_build/index.service.worker.js
+++ web_build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783210351|8533114474';
+const CACHE_VERSION = '1783212886|11068111253';
 /** @type {string} */
 const CACHE_PREFIX = 'Archipiélago Mai-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
