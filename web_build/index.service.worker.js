bbc7ecbe2e766bb387b01034ffd62bc4f9267e87
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2026-07-29 01:37:18 -0300

    Update file sizes in index.html and index.service.worker.js, and refresh binary assets in index.pck and index.wasm for improved performance and caching.

--- web_build/index.service.worker.js
+++ web_build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783571017|7523420959';
+const CACHE_VERSION = '1785299808|17651183409';
 /** @type {string} */
 const CACHE_PREFIX = 'Archipiélago Mai-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
