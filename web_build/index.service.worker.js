9cfeaf56dc5448231e838d5255d05bb9d14b7602
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2026-08-22 14:31:10 -0300

    Update Godot configuration, enhance audio resume functionality, and increment cache version.
    Modified the Godot configuration in index.html to reflect updated file sizes. Added a new function to resume audio playback on user interaction in index.js. Incremented the cache version in the service worker to ensure updated resources are fetched. Updated binary files for the latest build.

--- web_build/index.service.worker.js
+++ web_build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787110639|9024510654';
+const CACHE_VERSION = '1787419855|10050925603';
 /** @type {string} */
 const CACHE_PREFIX = 'Archipiélago Mai-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
