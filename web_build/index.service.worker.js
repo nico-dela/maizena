9d85ceee8b4d31dca41e53690398a3f57152468b
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2026-08-22 17:57:58 -0300

    Update Godot configuration and increment cache version for web build.
    Adjusted file sizes in the Godot configuration within index.html and updated the CACHE_VERSION in the service worker to ensure the latest resources are cached. Updated binary files for the latest build.

--- web_build/index.service.worker.js
+++ web_build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787431400|2826721620';
+const CACHE_VERSION = '1787432271|3698009458';
 /** @type {string} */
 const CACHE_PREFIX = 'Archipiélago Mai-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
